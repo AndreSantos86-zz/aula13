f25af82e558f2dcd04b7bb70943fac814c8ab84d
Andre Luis Santos <86andresantos@gmail.com>
2021-11-29 15:16:08 -0300

    Finalizado
    inserido imagens

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-alert('Olá')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var msg2 = window.document.getElementById('msg2')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    
+    if(hora >= 0 && hora < 12){
+    // bom dia
+    img.src = 'manha.png'
+    document.body.style.background = '#e2cd9f'
+    msg2.innerText='Bom dia'
+    }else if(hora >= 12 && hora < 18)
+    {
+    
+    img.src = 'tarde.png'
+    document.body.style.background = '#b9846f'
+    msg2.innerText = 'Boa Tarde'
+
+    }else{
+        //Boa noite
+        img.src= 'noite.png'
+        document.body.style.background = '#515154'
+        msg2.innerText = 'Boa Noite'
+    }
+}
